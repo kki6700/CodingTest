ad934bea5b757571344a844abf0ad9497434969a
Ulari <59388611+kki6700@users.noreply.github.com>
2025-06-16 09:45:49 +0900

    Update x만큼 간격이 있는 n개의 숫자.cpp

--- 프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.cpp
+++ 프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.cpp
@@ -8,10 +8,5 @@ vector<long long> solution(int x, int n) {
     for (int i = 1; i <= n; i++) {
         answer.push_back((long long)x * i); 
     }
-    
-    
-    ;
-    
-    
     return answer;
 }
