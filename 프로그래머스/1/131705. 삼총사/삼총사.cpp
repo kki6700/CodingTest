d52180b8af3d86a4c9b55e1d68a5760adc38fff9
Ulari <59388611+kki6700@users.noreply.github.com>
2025-09-11 16:28:28 +0900

    [level 1] Title: 삼총사, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+int solution(vector<int> number) {
+    int answer = 0;
+    int n = number.size();
+    
+    // 3중 for문으로 모든 조합 탐색
+    for (int i = 0; i < n - 2; i++) {
+        for (int j = i + 1; j < n - 1; j++) {
+            for (int k = j + 1; k < n; k++) {
+                if (number[i] + number[j] + number[k] == 0) {
+                    answer++;
+                }
+            }
+        }
+    }
+    return answer;
+}
